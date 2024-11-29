b21e8d2d6042b29b40ff3bc87bcd09c41b06e8b8
rhayssakramer <rhayssakramer@hotmail.com>
2024-11-29 13:17:22 -0300

    Criando método calcular

--- teste_unitario_angular/src/app/_services/calculadora.service.ts
+++ teste_unitario_angular/src/app/_services/calculadora.service.ts
@@ -6,4 +6,19 @@ import { Injectable } from '@angular/core';
 export class CalculadoraService {
 
   constructor() { }
+
+  calcular(num1: number, num2: number, operacao: string) {
+    switch (operacao) {
+      case 'soma':
+        return num1 + num2;
+      case 'subtração':
+        return num1 - num2;
+      case 'multiplicação':
+        return num1 * num2
+      case 'divisão':
+        return num1 / num2
+      default:
+        return null
+    }
+  }
 }
